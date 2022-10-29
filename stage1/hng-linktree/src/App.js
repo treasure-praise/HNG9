a91a2aa8e332d13bc2889575cfaae7c273ae748c
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-10-29 19:53:24 +0100

    created social section for github and slack

--- stage1/hng-linktree/src/App.js
+++ stage1/hng-linktree/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Footer from "./components/Footer";
+import SocialSection from "./components/SocialSection";
 
 function App() {
   return (
     <div className="App">
+      <SocialSection />
       <Footer />
     </div>
   );
---
+++ stage1/hng-linktree/src/components/SocialSection.jsx
@@ -0,0 +1,17 @@
+import Github from "../assets/Github.svg";
+import Slack from "../assets/slack.svg";
+
+const SocialSection = () => {
+  return (
+    <div className="flex justify-center my-8">
+      <a className="m-4" href="https://internship.zuri.team/hngi9">
+        <img src={Slack} alt="" srcset="" />
+      </a>
+      <a className="m-4" href="https://internship.zuri.team/hngi9">
+        <img src={Github} alt="" srcset="" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialSection;

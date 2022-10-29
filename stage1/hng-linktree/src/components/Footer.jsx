4ef958ff50174b955c12dc655d74f67d37d8181b
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-10-29 19:43:20 +0100

    created footer component

---
+++ stage1/hng-linktree/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import ZuriLogo from "../assets/Zuri.svg";
+import I4G from "../assets/I4G.svg";
+
+const Footer = () => {
+  return (
+    <footer className="md:flex justify-between border-t-2 mx-8">
+      <a className="m-4" href="https://internship.zuri.team/hngi9">
+        <img src={ZuriLogo} alt="" srcset="" />
+      </a>
+      <p className="text-left my-4">HNG Internship 9 Frontend Task</p>
+      <a className="m-4" href="https://internship.zuri.team/hngi9">
+        <img src={I4G} alt="" srcset="" />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;

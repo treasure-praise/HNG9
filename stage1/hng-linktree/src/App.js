d3ef78c8b2183d66804bda5e4c4baaabcded0515
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-11-04 00:35:42 +0100

    style:implemented contact page design

--- stage1/hng-linktree/src/App.js
+++ stage1/hng-linktree/src/App.js
@@ -3,13 +3,15 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import LinksSection from "./components/LinksSection";
 import SocialSection from "./components/SocialSection";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div>
+      {/* <Header />
       <LinksSection />
-      <SocialSection />
+      <SocialSection /> */}
+      <Contact />
       <Footer />
     </div>
   );
---
+++ stage1/hng-linktree/src/pages/Contact.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="w-5/6 mt-16 md:w-1/2 mx-auto">
+      <h1 className="text-3xl mb-4">Contact Me</h1>
+      <p className=" mb-12">
+        Hi there, contact me to ask me about anything you have in mind
+      </p>
+      <div>
+        <form action="" className="flex flex-col">
+          <div className="md:flex gap-6">
+            <div className="flex flex-col md:w-1/2">
+              <label className="mb-2" htmlFor="">
+                First Name
+              </label>
+              <input
+                className="border-[#D0D5DD] border rounded-lg px-3 py-2 mb-6"
+                type="text"
+                name="firstName"
+                id="firstName"
+                placeholder="Enter your first name"
+              />
+            </div>
+
+            <div className="flex flex-col md:w-1/2">
+              <label className="mb-2" htmlFor="">
+                Last Name
+              </label>
+              <input
+                className="border-[#D0D5DD] border rounded-lg px-3 py-2 mb-6"
+                type="text"
+                name="laststName"
+                id="laststName"
+                placeholder="Enter your last name"
+              />
+            </div>
+          </div>
+
+          <label className="mb-2" htmlFor="">
+            Email
+          </label>
+          <input
+            className="border-[#D0D5DD] border rounded-lg px-3 py-2 mb-6"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="yourname@email.com"
+          />
+
+          <label className="mb-2" htmlFor="">
+            Message
+          </label>
+          <textarea
+            className="border-[#D0D5DD] border rounded-lg px-3 py-2 mb-6"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+          ></textarea>
+
+          <div className="flex items-center gap-3 mb-8">
+            <input type="checkbox" name="" id="" className="rounded-md" />
+            <label htmlFor="">
+              You agree to providing your name to Treasure-Praise who may
+              contact you
+            </label>
+          </div>
+
+          <button
+            className="bg-[#1570EF] font-bold text-white rounded-lg py-3"
+            type="submit"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;

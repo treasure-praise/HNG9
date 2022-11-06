09790a98dbe04ee43bb7add9fcad4ca80c8eeb5a
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-11-06 12:52:34 +0100

    style:added necessary ids for testing

--- stage1/hng-linktree/src/components/NavLink.jsx
+++ stage1/hng-linktree/src/components/NavLink.jsx
@@ -5,7 +5,7 @@ const NavLink = ({ text, link, id, routerLink }) => {
   return (
     <div className="w-full border hover:bg-[#D0D5DD] bg-[#F4F5F7] rounded-md my-6">
       {routerLink && (
-        <Link to="/contact">
+        <Link id={id} to="/contact">
           <h1 className="py-6">{text}</h1>
         </Link>
       )}
